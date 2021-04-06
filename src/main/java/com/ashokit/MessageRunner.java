ceca0d7c36df86d39abe9b29cc61c3d76550c622
PC <PC@DESKTOP-39HMRIT>
2021-04-06 09:14:33 +0530

    WH-995 third commit time

--- src/main/java/com/ashokit/MessageRunner.java
+++ src/main/java/com/ashokit/MessageRunner.java
@@ -1,15 +1,22 @@
 package com.ashokit;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 @Component
 public class MessageRunner implements CommandLineRunner {
+	 private static Logger log = LoggerFactory.getLogger(MessageRunner.class);
 
 	@Override
 	public void run(String... args) throws Exception {
 		int a =10;
+		log.info("started");
+		
 		System.out.println("WLCOME TO SPRING BOOTAPP");
 		System.out.println("hello git");
+		log.info("end");
 
 		// TODO Auto-generated method stub
 
