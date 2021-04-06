bc1cd4b72966ad1ec3f448a462ff0991c841475c
PC <PC@DESKTOP-39HMRIT>
2021-04-06 08:16:06 +0530

    WH-8520 - First Time File

---
+++ src/main/java/com/ashokit/MessageRunner.java
@@ -0,0 +1,15 @@
+package com.ashokit;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("WLCOME TO SPRING BOOTAPP");
+		// TODO Auto-generated method stub
+
+	}
+
+}
